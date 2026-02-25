22b880ea562d71d3079c7cbec8eb8b3a201c3014
shawn <agst.tech2@gmail.com>
2026-02-25 19:51:25 +0900

    간편 구매,판매등록 작업

---
+++ src/app/services/regBuyCoin/page.tsx
@@ -0,0 +1,28 @@
+import PageBanner from "@/components/Common/PageBanner";
+import Footer from "@/components/Layout/Footer";
+import Navbar from "@/components/Layout/Navbar";
+import ServicesLists from "@/components/Services/ServicesLists";
+import MoneyTransferRoutes from "@/components/Common/MoneyTransferRoutes";
+import CurrencyConverter from "@/components/MoneyTransfer/CurrencyConverter";
+import React from "react";
+import BusinessPayments from "@/components/Business/BusinessPayments";
+import ExpressRegisterBuyCoin from "@/components/ExpressBuySellCoin/ExpressRegisterBuyCoin";
+
+export default function Page() {
+  return (
+    <>
+      <Navbar />
+
+      <PageBanner
+        homePageUrl="/"
+        homePageText="Home"
+        pageTitle="간편 구매 등록"
+        phoneNumber="+44 (0506) 258 7485"
+        email="support24@libor-otc.com"
+      />
+        <ExpressRegisterBuyCoin />
+
+      <Footer />
+    </>
+  );
+}
