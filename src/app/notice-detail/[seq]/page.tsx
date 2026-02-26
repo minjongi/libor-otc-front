351cd54b5e85bf0d62c65996b14dc71410e08c23
shawn <agst.tech2@gmail.com>
2026-02-26 22:42:12 +0900

    공지사항 제목 클릭시 해당 상세페이지 이동하는 페이지 작업

---
+++ src/app/notice-detail/[seq]/page.tsx
@@ -0,0 +1,25 @@
+import PageBanner from "@/components/Common/PageBanner";
+import Footer from "@/components/Layout/Footer";
+import Navbar from "@/components/Layout/Navbar";
+import NoticeDetailForm from "@/components/Notice-detail/Notice-detail";
+
+
+export default function Page() {
+    return (
+        <>
+            <Navbar/>
+
+            <PageBanner
+                homePageUrl="/"
+                homePageText="Home"
+                pageTitle="공지사항"
+                phoneNumber="+44 (0506) 258 7485"
+                email="support24@libor-otc.com"
+            />
+
+            <NoticeDetailForm />
+
+            <Footer/>
+        </>
+    );
+}
---
+++ src/components/Notice-detail/Notice-detail.tsx
@@ -0,0 +1,96 @@
+'use client'
+
+import React, {useEffect, useState} from "react";
+import {useParams} from "next/navigation";
+import {noticeDetail} from "@/utils/customerUtil";
+import Link from "next/link";
+
+const NoticeDetailForm = () => {
+
+    const params = useParams()
+    const seq = params.seq
+
+    const [detail, setDetail] = useState({title: '', createdAt: '' ,content: ''})
+
+    useEffect(() => {
+        const fetchData = async () => {
+            // const data = await noticeDetail(seq)
+
+            // setDetail(data)
+        }
+
+        if (seq) fetchData()
+    }, [seq])
+
+    const detailTem = {
+        title: "공지입니다. 필독",
+        createdAt: "2025-03-22 15:33:21",
+        content: "안녕하세요. 공지를 하겠습니다. 하나 둘 셋 넷 다섯 여섯 일곱 여덟 아홉 열까지 세어보겠습니다."
+    }
+
+    return (
+        <>
+            <div className="build-your-career-area ptb-120">
+                <div className="container">
+                    <div className="row justify-content-center g-4">
+                        <div className="col-lg-7">
+                            <div className="contact-us-form mw-648">
+                                <h3>공지사항</h3>
+
+                                <form>
+                                    <div className="currency-input position-relative z-1">
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            value={detailTem.title}
+                                            disabled
+                                            style={{
+                                                height: "60px",
+                                                padding: "0 15px"
+                                            }}
+                                        />
+                                    </div>
+
+                                    <div className="currency-input position-relative z-1">
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            value={detailTem.createdAt}
+                                            disabled
+                                            style={{
+                                                height: "60px",
+                                                padding: "0 15px"
+                                            }}
+                                        />
+                                    </div>
+
+                                    <div className="currency-input position-relative z-1">
+                                        <textarea
+                                            rows={5}
+                                            className="form-control"
+                                            value={detailTem.content}
+                                            disabled
+                                            style={{
+                                                height: "500px",
+                                                lineHeight: "1.5",
+                                                padding: "10px 15px"
+                                            }}
+                                        ></textarea>
+                                    </div>
+
+                                    <div className="btn btn-secondary w-100">
+                                        <Link href="/notice">
+                                            이전
+                                        </Link>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default NoticeDetailForm;
