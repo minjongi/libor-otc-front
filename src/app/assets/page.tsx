e1062b3ddffc898d6666ce20adc2a98846daf471
shawn <agst.tech2@gmail.com>
2026-02-28 18:09:33 +0900

    내자산 (작업 미완료, 곧 출시된다는 페이지만 설정)

---
+++ src/app/assets/page.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function Page() {
+    return (
+        <>
+            <div className="ptb-120">
+                <div className="container">
+                    <div className="text-center">
+                        <div className="mb-4">
+                            <Image
+                                src="/images/libor-logo.png"
+                                alt="libor"
+                                width={232}
+                                height={50}
+                            />
+                        </div>
+
+                        <h2 className="mb-2">COMING SOON</h2>
+                        <p>We’re putting the finishing touches on our new site.</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
