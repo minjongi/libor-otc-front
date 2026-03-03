9856b342e089ffad6f3c53396f3d28df3052cd57
shawn <agst.tech2@gmail.com>
2026-03-03 23:20:30 +0900

    내역 - 매칭 내역 작업

--- src/app/p2pTrade/matching/[id]/page.tsx
+++ src/app/p2pTrade/matching/[id]/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-import Link from "next/link";
 import Navbar from "@/components/Layout/Navbar";
 import PageBanner from "@/components/Common/PageBanner";
 import Footer from "@/components/Layout/Footer";
-import P2pTradeMatching from "@/components/P2pTrade-Matching/P2pTrade-Matching";
+import P2pTradeMatchingDetail from "@/components/P2pTrade-Matching/P2pTrade-Matching-detail";
 
 export default function Page() {
     return (
@@ -13,12 +11,12 @@ export default function Page() {
             <PageBanner
                 homePageUrl="/"
                 homePageText="Home"
-                pageTitle="내역"
+                pageTitle="거래정보"
                 phoneNumber="+44 (0506) 258 7485"
                 email="support24@libor-otc.com"
             />
 
-            <P2pTradeMatching />
+            <P2pTradeMatchingDetail />
 
             <Footer/>
         </>
